24f5bdb5b6c018a7efcd83376bd1674ff18cf791
Eduardo-guzmanv <eduardo.guzmanv@iteso.mx>
2026-09-22 20:51:32 -0600

    Create state for starting count and variable for counting states

--- p1_tm2_wireless_examples_ieee_802_15_4_mwa_end_device_freertos/source/mwa_end_device.c
+++ p1_tm2_wireless_examples_ieee_802_15_4_mwa_end_device_freertos/source/mwa_end_device.c
@@ -169,6 +169,14 @@ NVM_RegisterDataSet(&mAddrMode,           1,   sizeof(addrModeType_t), mAddrMode
 /* The current state of the applications state machine */
 uint8_t gState;
 
+//variables creadas para la practica
+typedef enum{
+	OFF,
+	ON
+}t_counter_state;
+
+uint8_t time_counter;
+t_counter_state counter_state = OFF;
 /************************************************************************************
 *************************************************************************************
 * Public functions
@@ -553,6 +561,8 @@ void AppThread(osaTaskParam_t argument)
                             TMR_StartLowPowerTimer(mTimer_c, gTmrSingleShotTimer_c ,mPollInterval, AppPollWaitTimeout, NULL );
                             /* Go to the listen state */
                             gState = stateListen;
+                            time_counter =0;
+                            counter_state=ON;
                             OSA_EventSet(mAppEvent, gAppEvtDummyEvent_c); 
                         }        
                         else 
